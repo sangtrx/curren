10fc34bca9573accf334c92d9f562d183c31c130
Quang Sang Truong <45520864+sangtrx@users.noreply.github.com>
2026-08-23 18:49:32 +0700

    feat: add public Python package

---
+++ src/curren/__init__.py
@@ -0,0 +1,14 @@
+from curren.client import CurrenClient, CurrenError
+from curren.models import LifecycleEvent, Signal, SignalList, TrackRecord, VerificationRecord
+
+__all__ = [
+    "CurrenClient",
+    "CurrenError",
+    "LifecycleEvent",
+    "Signal",
+    "SignalList",
+    "TrackRecord",
+    "VerificationRecord",
+]
+
+__version__ = "0.1.0"
